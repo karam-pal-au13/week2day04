1179841c5dfcf1d104140f5c3d768bb7eaeb3dee
karam <karamsangwan19@gmail.com>
2020-09-04 00:56:07 +0700

    all done

---
+++ coding-challenge/day04cc.py
@@ -0,0 +1,11 @@
+print ("coding challenge day04", end= " ") 
+a = 4
+square = a*a
+print ("square of 4 is", square)
+b = 2
+square = b*b
+print ("square of 2 is", square)
+c = 6 
+square = c*c
+print ("square of 6 is", square)
+print ("done successfully", end= " ")
